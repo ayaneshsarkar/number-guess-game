ca91f8f6c0791188b8c0377ec8abf57e721ad121
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2022-02-10 00:40:39 +0530

    Initial Commit :heavy_check_mark:

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import DefaultStyles from '../constants/default-styles';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import MainButton from '../components/MainButton';
+import BodyText from '../components/BodyText';
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -17,16 +19,24 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 }
 
+const renderListItem = (listLength, itemData) => (
+  <View style={styles.listItem}>
+    <BodyText>#{ listLength - itemData.index }</BodyText>
+    <BodyText>{ itemData.item }</BodyText>
+  </View>
+);
+
 const GameScreen = ({ userChoice, onGameOver }) => {
-  const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, userChoice));
-  const [rounds, setRounds] = useState(0);
+  const initialGuess = generateRandomBetween(1, 100, userChoice);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [passGuesses, setPassGuesses] = useState([ initialGuess.toString() ]);
 
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
   useEffect(() => {
     if(currentGuess === userChoice) {
-      onGameOver(rounds);
+      onGameOver(passGuesses.length);
     }
   }, [ currentGuess, userChoice, onGameOver ]);
 
@@ -45,7 +55,7 @@ const GameScreen = ({ userChoice, onGameOver }) => {
     if(direction === 'lower') {
       currentHigh.current = currentGuess;
     } else {
-      currentLow.current = currentGuess;
+      currentLow.current = currentGuess + 1;
     }
 
     const nextNumber = generateRandomBetween(
@@ -53,7 +63,8 @@ const GameScreen = ({ userChoice, onGameOver }) => {
     );
 
     setCurrentGuess(nextNumber);
-    setRounds(curRounds => curRounds + 1);
+    // setRounds(curRounds => curRounds + 1);
+    setPassGuesses(curPastGuesses => [ nextNumber.toString(), ...curPastGuesses ])
   }
 
   return (
@@ -61,9 +72,25 @@ const GameScreen = ({ userChoice, onGameOver }) => {
       <Text style={DefaultStyles.title}>Opponent's guess</Text>
       <NumberContainer>{ currentGuess }</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>LOWER</MainButton>
-        <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>GREATER</MainButton>
+        <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+          <Ionicons name="md-remove" size={25} color="white" />
+        </MainButton>
+        <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+        <Ionicons name="md-add" size={25} color="white" />
+        </MainButton>
       </Card>
+      <View style={styles.listContainer}>
+        {/* <ScrollView contentContainerStyle={styles.list}>
+          { passGuesses.map((guess, index) => renderListItem(guess, passGuesses.length - index)) }
+        </ScrollView> */}
+
+        <FlatList 
+          keyExtractor={item => item}
+          data={passGuesses} 
+          renderItem={renderListItem.bind(this, passGuesses.length)} 
+          contentContainerStyle={styles.list}
+        />
+      </View>
     </View>
   )
 }
@@ -80,6 +107,25 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: '90%'
+  },
+  listContainer: {
+    flex: 1,
+    width: '61%'
+  },
+  list: {
+    flexGrow: 1,
+    // alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  listItem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
   }
 })
 
